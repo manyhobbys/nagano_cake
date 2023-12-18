428367a8b97af2a6a10dde69722cd0f1de91742b
n-haruhi <hr23nakanishi@gmail.com>
2023-12-18 10:46:49 +0000

    [Update]Orderモデルにenum追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ApplicationRecord
+
   belongs_to :customer
-  has_many :order_details, dependent: :denstroy
+  has_many :order_details, dependent: :destroy
+  has_many :items, through: :order_details
+
+  enum payment_method: { credit_card: 0, transfer: 1}
+  enum status: {wait_for_payment: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4}
 end
