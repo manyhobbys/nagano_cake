cfa062dbfb5652d4a632fb78f2fd4b85bd844f98
nyoonn3902 <shohe.k0000@gmail.com>
2023-12-18 15:16:22 +0000

    customer/edit,customer_controller

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,12 +1,29 @@
 class Public::CustomersController < ApplicationController
   def show
-    @user = current_customer
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    @customer = update(customer_params)
+    if @customer = save
+      redirect_to  customers_my_page_path
+    else
+      render :edit
+    end
   end
 
   def unsubscribe
   end
+
+private
+  def customer_params
+    params.require(:customer).permit(:email,:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:telephone_number )
+  end
+
 
 end
