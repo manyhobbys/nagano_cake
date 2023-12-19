f48fe4057ea7944d68731b1d14531a72e27d90a3
n-haruhi <hr23nakanishi@gmail.com>
2023-12-19 09:38:45 +0000

    [Update]製作ステータスの更新記述

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,25 @@
 class Admin::OrderDetailsController < ApplicationController
+
+  def update
+    order_detail = OrderDetail.find(params[:order_id])
+    order_detail.production_status = params[:order_detail][:making_status]
+    order_details = OrderDetail.where(order_id: order_detail.order_id)
+    if order_detail.production_status == "in_making"
+      order_detail.order.update(status: 2)
+    end
+    if order_detail.update(order_detail_params)
+      redirect_to admin_order_path
+    end
+    if order_details.all? { |detail| detail.production_status == "making_complete" }
+      order_detail.order.update(status: 3)
+    end
+  end
+
+
+  private
+
+  def order_detail_params
+    params.require(:order_detail).permit(:production_status)
+  end
+
 end
