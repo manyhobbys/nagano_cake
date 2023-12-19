c4631d18bc700181f9d5ca9bb28b5dbc9b57f38a
terukouki07 <azumacha08@gmail.com>
2023-12-19 08:53:30 +0000

    itemページの作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -6,7 +6,7 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     @item.save
-    redirect_to admin_item(@item.id)
+    redirect_to admin_item_path(@item)
   end
 
   def index
@@ -23,15 +23,16 @@ class Admin::ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    if @item.update(item_paramas)
-       redirect_to admin_item_path(@item.id)
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item)
     else
       render :edit
+    end
   end
 
   private
 
   def item_params
-    params.requier(:item).permit(:name, :introduction, :price, :is_active)
+    params.require(:item).permit(:name, :introduction, :price, :is_active, :image, :genre_id)
   end
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,7 +3,7 @@ class Public::ItemsController < ApplicationController
     @item = Item.page(params[:page]).per(8)
     @items = Item.all
   end
-  
+
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
