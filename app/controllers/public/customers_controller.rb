e92c6af56b30cbc6ce211bc9ad1036672a0b35b1
nyoonn3902 <shohe.k0000@gmail.com>
2023-12-19 04:54:38 +0000

    退会機能実装

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -20,6 +20,15 @@ class Public::CustomersController < ApplicationController
   def unsubscribe
   end
 
+  def withdraw
+    @customer = Customer.find(current_customer.id)
+    @customer.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行いたしました"
+    redirect_to root_path
+
+  end
+
 private
   def customer_params
     params.require(:customer).permit(:email,:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:telephone_number )
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   before_action :configure_permitted_parameters
+  before_action :reject_customer, only: [:create]
 
   def after_sign_in_path_for(resource)
     customers_my_page_path
@@ -17,10 +18,24 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  protected
+
+protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email,])
   end
 
- 
+  def reject_customer
+    @customer = Customer.find_by(email: params[:customer][:email])
+    if @customer
+      if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == true)
+        flash[:notice] = "退会済みです。再度ご登録をしてご利用ください"
+        redirect_to new_customer_registration_path
+      else
+        flash[:notice] = "項目を入力してください"
+      end
+    else
+      flash[:notice] = "該当するユーザーが見つかりません"
+    end
+  end
+
 end
