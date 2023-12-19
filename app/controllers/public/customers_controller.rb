ae0d8f83acfc14943d4a31f0a5b6829aea6e3bfb
nyoonn3902 <shohe.k0000@gmail.com>
2023-12-19 06:45:39 +0000

    退会機能実装

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -20,7 +20,13 @@ class Public::CustomersController < ApplicationController
   def unsubscribe
   end
 
-  
+  def withdraw
+    @customer = Customer.find(current_customer.id)
+    @customer.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行いたしました"
+    redirect_to root_path
+  end
 
 private
   def customer_params
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -27,7 +27,7 @@ protected
   def reject_customer
     @customer = Customer.find_by(email: params[:customer][:email])
     if @customer
-      if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == true)
+      if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == false)
         flash[:notice] = "退会済みです。再度ご登録をしてご利用ください"
         redirect_to new_customer_registration_path
       else
