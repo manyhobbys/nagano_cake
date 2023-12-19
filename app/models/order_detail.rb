6161621375ecd46850bfa909e1de24956c44e61e
n-haruhi <hr23nakanishi@gmail.com>
2023-12-19 04:17:48 +0000

    [Update]order_detailモデル追記

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,6 +1,12 @@
 class OrderDetail < ApplicationRecord
   belongs_to :item
   belongs_to :order
-  
-  
+
+  def subtotal
+    self.including_tax_price * self.amount
+  end
+
+# 0:製作不可, 1:製作待ち, 2:製作中, 3:製作完了
+  enum making_status:{ start_not_possible: 0, making_pending: 1, in_making:2, making_complete: 3 }
+
 end
