413f338cd91e7971ac09bbaec2a0b74e3b70976a
nyoonn3902 <shohe.k0000@gmail.com>
2023-12-19 06:04:19 +0000

    ヘッダー、退会機能削除

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -20,14 +20,7 @@ class Public::CustomersController < ApplicationController
   def unsubscribe
   end
 
-  def withdraw
-    @customer = Customer.find(current_customer.id)
-    @customer.update(is_deleted: true)
-    reset_session
-    flash[:notice] = "退会処理を実行いたしました"
-    redirect_to root_path
-
-  end
+  
 
 private
   def customer_params
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,8 +8,6 @@ class Customer < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :addresses, dependent: :destroy
   
-  def active_for_authentication?
-    super && (is_deleted == false)
-  end
+  
   
 end
