31d2bd711ac67b5eb8a89e9ceea2b018e1c4384f
terukouki07 <azumacha08@gmail.com>
2023-12-19 12:50:51 +0000

    [finish]item作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,8 +14,11 @@ class Item < ApplicationRecord
     image.variant(resize_to_limit: [width, height]).processed
   end
 
+  def add_tax_price
+    (self.price * 1.10).round
+  end
+
   validates :name, presence: true
   validates :introduction, presence: true
   validates :price, presence: true
-  validates :is_active, presence: true
 end
