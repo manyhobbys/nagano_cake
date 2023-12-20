8aaa9407a0c414d8f1fea5e8e0131c6db167b963
terukouki07 <azumacha08@gmail.com>
2023-12-20 16:52:19 +0000

    [finish]レイアウト

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,11 +9,9 @@ class Item < ApplicationRecord
   def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      # file_path = Rails.root.join('app/assets/images')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     # image.variant(resize_to_limit: [width, height]).processed
-    # image.variant(gravity: :center, resize:"200x100^", crop:"200x100+0+0").processed
     image.variant(gravity: :center, resize:"#{width}x#{height}^", crop:"#{width}x#{height}+0+0").processed
   end
 
