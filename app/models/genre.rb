885634b935070dd304ebe35f93b2f145f5e29223
n-haruhi <hr23nakanishi@gmail.com>
2023-12-20 03:38:35 +0000

    [Update]注文,オーダーレイアウト編集

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,5 +2,5 @@ class Genre < ApplicationRecord
 
   has_many :items, dependent: :destroy
 
-  varidates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 end
